c482227a83bfbd36b72a8d6cca99f1bf922bda3c
BenoitValdes <valdes.benoit@gmail.com>
2020-10-16 17:20:34 +0200

    add the context items

--- cix.py
+++ cix.py
@@ -3,6 +3,7 @@ DISCLAIMER: This repository and it's development is not linked to ISOTROPIX in a
 
 @Author: Benoit Valdes
 """
+import ix
 from ix import *
 import items_class
 reload(items_class)
@@ -19,11 +20,15 @@ def get_item(path):
     map_dict = {
         "Attribute": items_class.attribute_class.Attribute,
         "SceneItem": items_class.SceneItem,
+        "Context": items_class.Context,
     }
     
     ix_node = ix.get_item(str(path))
+    
     if "get_type" in  dir(ix_node):
         kindof = "Attribute"
+    elif ix_node.is_context() is True:
+        kindof = "Context"
     else:
         kindof = ix_node.get_class_name()
         if not kindof in map_dict:
@@ -31,3 +36,6 @@ def get_item(path):
 
     # TODO: Check the kindof item it is an return the right Wrapper Class
     return map_dict[kindof](ix_node)
+
+def get_current_context():
+    return get_item(ix.get_current_context())
--- items_class.py
+++ items_class.py
@@ -1,10 +1,12 @@
+import ix
+import cix
 import wrapper
 reload(wrapper)
 import attribute_class
 reload(attribute_class)
 
 
-class SceneItem(wrapper.Wrapper):
+class ProjectItem(wrapper.Wrapper):
     """
     This Class wrap Clarisse items one to be OOP friendly.
     To avoid any conflict with Clarisse item's attributes, all the function had to have a prefix.
@@ -15,7 +17,7 @@ class SceneItem(wrapper.Wrapper):
     """
 
     def __init__(self, ix_node):
-        super(SceneItem, self).__init__(ix_node)
+        super(ProjectItem, self).__init__(ix_node)
         self._attr_list = []
 
         # Add all item's attributes as class properties
@@ -23,22 +25,6 @@ class SceneItem(wrapper.Wrapper):
             attr = attribute_class.Attribute(self._node.get_attribute(i))
             self._attr_list.append(attr)
             setattr(self, attr.get_name(), attr)
-
-    def add_custom_attribute(self, group_name, attr_name, kindof, array_length=1):
-        """
-        Add a custom attribute to the item.
-
-        Args:
-            group_name (str): Group name in the Attribute Editor (created if it doesn't exists)
-            attr_name (str): Name of the attribute
-            kindof (str): Type of attribute (string, filename, double, bool...)
-            array_length (int, optional): For some attribute type (double, long...) you can specify an array to
-                                          represent your attribute. Defaults to 1.
-
-        Raises:
-            AttributeError: It's not implemented yet so it raise an error because you can't use it.
-        """
-        raise AttributeError("Need to be implemented")
 
     def get_attribute_list(self):
         """
@@ -57,7 +43,82 @@ class SceneItem(wrapper.Wrapper):
         Returns:
             list[str]: The list of all the attributes availlable of this Class
         """
-        result = super(SceneItem, self).__dir__()
+        result = super(ProjectItem, self).__dir__()
         result += [attr.get_name() for attr in self._attr_list]
     
+        return result
+        
+class SceneItem(ProjectItem):
+    """
+    This Class wrap Clarisse items one to be OOP friendly.
+    To avoid any conflict with Clarisse item's attributes, all the function had to have a prefix.
+    ex: is, get, set, add
+
+    If the module is in debug mode (__builtins__["cix_is_debug"]=True) a test function will be
+    executed at the end of the init.
+    """
+
+    def __init__(self, ix_node):
+        super(SceneItem, self).__init__(ix_node)
+
+    def add_custom_attribute(self, group_name, attr_name, kindof, array_length=1):
+        """
+        Add a custom attribute to the item.
+
+        Args:
+            group_name (str): Group name in the Attribute Editor (created if it doesn't exists)
+            attr_name (str): Name of the attribute
+            kindof (str): Type of attribute (string, filename, double, bool...)
+            array_length (int, optional): For some attribute type (double, long...) you can specify an array to
+                                          represent your attribute. Defaults to 1.
+
+        Raises:
+            AttributeError: It's not implemented yet so it raise an error because you can't use it.
+        """
+        raise AttributeError("Need to be implemented")
+
+class Context(ProjectItem):
+    """
+    This Class wrap Clarisse items one to be OOP friendly.
+    To avoid any conflict with Clarisse item's attributes, all the function had to have a prefix.
+    ex: is, get, set, add
+
+    If the module is in debug mode (__builtins__["cix_is_debug"]=True) a test function will be
+    executed at the end of the init.
+    """
+
+    def __init__(self, ix_node):
+        super(Context, self).__init__(ix_node)
+
+    def get_children(self, ctx_only=False, items_only=False, recursive=False):
+        """
+        Get the childrens of the context.
+
+        Args:
+            ctx_only (bool, optional): If True, only return contexts (not compatible with `items_only`). Defaults to False.
+            items_only (bool, optional): If True, return everything else than context (not compatible with `ctx_only`). Defaults to False.
+            recursive (bool, optional): If True, go through the sub contexts. Defaults to False.
+
+        Returns:
+            [type]: [description]
+        """
+        result = []
+        objects = ix.api.OfItemVector()
+        function = None
+        if recursive:
+            function = self._node.get_all_items
+        else:
+            function = self._node.get_items
+
+        function(objects)
+
+        for i in range(objects.get_count()):
+            item = objects[i]
+
+            if item.is_context() is True and (ctx_only is True or items_only is False):
+                result.append(cix.get_item(item))
+
+            elif item.is_context() is not True and (ctx_only is False or items_only is True):
+                result.append(cix.get_item(item))
+
         return result
