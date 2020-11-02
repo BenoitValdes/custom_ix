9728513ea7c797cd5ce83a64910518f64faf40c0
BenoitValdes <valdes.benoit@gmail.com>
2020-11-02 14:35:37 +0100

    edit header description of the module

--- cix.py
+++ cix.py
@@ -1,6 +1,16 @@
 """
 DISCLAIMER: This repository and it's development is not linked to ISOTROPIX in any manear.
 
+This module is meant to wrap Clarisse API in a way it's easier for a user used to python or other DCC API like Maya to work with Clarisse API.
+CIX means Custom IX. There is almost no override of IX API, it's just a different way to access and manipulate Clarisse Items.
+The bigest part is to deal with Clarisse Nodes and allow fast access to attributes, get the connections between nodes or even access to rows of shading layers in an
+elegant python way.
+
+Clarisse original API is a bit hard to work with at the begining but is very powerfull because it allow the user to do amlmost everything than C++ can do.
+This is the reason why this wrapper override a few thing of the original API and allow you to get back the the original API whenever you need to.
+
+You can work with original Clarisse API and then pass Clarisse items to CIX API if you need easy to use functions or method to do something faster than what IX allow to do.
+
 @Author: Benoit Valdes
 """
 import ix
@@ -31,7 +41,7 @@ def get_item(path, silent=False):
         else:
             ix.log_warning("`{}` is not a valid Clarisse item and/or can't be found".format(path))
             return None
-                
+
     if "get_type" in  dir(ix_node):
         kindof = "Attribute"
     elif ix_node.is_context() is True:
@@ -44,7 +54,6 @@ def get_item(path, silent=False):
     # TODO: Check the kindof item it is an return the right Wrapper Class
     return map_dict[kindof](ix_node)
 
-
 def get_current_context():
     """override of ix.get_current_context() to return wrapped class
 
@@ -52,7 +61,6 @@ def get_current_context():
         Context: the current context your are in Clarisse
     """
     return get_item(ix.get_current_context())
-
 
 def reference_file(path, parent_ctx=get_current_context()):
     """
